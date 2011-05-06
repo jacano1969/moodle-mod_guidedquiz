1973a7edecd0c1e90896ec21c5f0eab50f0507ad
David Monllaó <david.monllao@gmail.com>
2011-05-06 07:48:17 +0200

    lang

---
+++ lang/ca_utf8/guidedquiz.php
@@ -0,0 +1,16 @@
+<?php 
+
+$string['assignvars'] = 'Assignar variables';
+$string['correctanswer'] = 'Resposta correcta';
+$string['errornoargs'] = 'No hi ha arguments de tipus qüestionari guiat en aquesta pregunta';
+$string['errornoquizvars'] = 'El qüestionari solicitat no conté variables';
+$string['finishattemptwithoutanswerlastquestion'] = 'Deixa la pregunta sense respondre i finalitza';
+$string['modulename'] = 'Qüestionari guiat';
+$string['modulenameplural'] = 'Qüestionaris guiats';
+$string['nextquestion'] = 'Següent pregunta';
+$string['nextquestionwithoutanswer'] = 'Deixa sense contestar i pasa a la següent pregunta';
+$string['questionpenalty'] = 'Penalització';
+$string['questionattempts'] = 'Intents';
+$string['remainingattempts'] = 'Intents restants';
+$string['viewpreviousquestions'] = 'Veure preguntes previament contestades';
+$string['showcorrectresponses'] = 'Mostrar resposta/es correcta/es';
---
+++ lang/es_utf8/guidedquiz.php
@@ -0,0 +1,16 @@
+<?php 
+
+$string['assignvars'] = 'Assignar variables';
+$string['correctanswer'] = 'Respuesta/s correcta/s';
+$string['errornoargs'] = 'No hay argumentos de tipo cuestionario guiado en esta pregunta';
+$string['errornoquizvars'] = 'El cuestionario solicitado no tiene variables';
+$string['finishattemptwithoutanswerlastquestion'] = 'Deja la pregunta en blanco y finaliza';
+$string['modulename'] = 'Cuestionario guiado';
+$string['modulenameplural'] = 'Cuestionarios guiados';
+$string['nextquestion'] = 'Siguiente pregunta';
+$string['nextquestionwithoutanswer'] = 'Siguiente pregunta sin contestar';
+$string['questionpenalty'] = 'Penalización';
+$string['questionattempts'] = 'Intentos';
+$string['remainingattempts'] = 'Intentos restantes';
+$string['viewpreviousquestions'] = 'Ver preguntas ya contestadas';
+$string['showcorrectresponses'] = 'Mostrar respuesta/s correcta/s';
