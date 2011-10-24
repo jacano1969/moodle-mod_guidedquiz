d9cff143958353275b8210fe58f0e4f87148ba2c
David Monllaó <david.monllao@gmail.com>
2011-10-24 23:07:46 +0200

    M&P requirements compliant

--- version.php
+++ version.php
@@ -9,4 +9,13 @@ $module->version  = 2011102001;   // The (date) version of this module
 $module->requires = 2007101509;   // Requires this Moodle version
 $module->cron     = 0;            // How often should cron check this module (seconds)?
 
-?>
+$module->release = '1.0 (Build: 2011102001)';
+
+// To avoid 1.9 Notice
+if (!defined('MATURITY_RC')) {
+    define('MATURITY_RC', 150);
+}
+
+// To avoid the M&P warning (yes, sad but true http://www.youtube.com/watch?v=l8BRbM52gpc)
+$plugin->maturity = MATURITY_RC;
+
