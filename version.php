9636c242bcaa98eff416717285fab151adf02826
David Monllaó <david.monllao@gmail.com>
2012-01-17 10:11:41 +0100

    Solving problem when installing

--- version.php
+++ version.php
@@ -5,11 +5,11 @@
 //  This fragment is called by moodle_needs_upgrading() and /admin/index.php
 ////////////////////////////////////////////////////////////////////////////////
 
-$module->version  = 2011102001;   // The (date) version of this module
+$module->version  = 2012011700;   // The (date) version of this module
 $module->requires = 2007101509;   // Requires this Moodle version
 $module->cron     = 0;            // How often should cron check this module (seconds)?
 
-$module->release = '1.4(Build: 2011102001)';
+$module->release = '1.4(Build: 2012011700)';
 
 // To avoid 1.9 Notice
 if (!defined('MATURITY_RC')) {
